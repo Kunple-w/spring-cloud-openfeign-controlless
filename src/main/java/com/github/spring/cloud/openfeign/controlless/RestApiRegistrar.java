48906b8233db481a40ff11650db0c7665cb26f92
wangyongxu <wangyx1122@gmail.com>
2021-07-16 23:18:12 +0800

    feat(controlless): RestApi bean注册器

---
+++ src/main/java/com/github/spring/cloud/openfeign/controlless/RestApiRegistrar.java
@@ -0,0 +1,34 @@
+package com.github.spring.cloud.openfeign.controlless;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * @author wangyongxu
+ */
+public class RestApiRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware, EnvironmentAware {
+    private Environment environment;
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+
+        // TODO: 2021-07-16 11:16:48 字节码增强? by wangyongxu
+
+    }
+}
